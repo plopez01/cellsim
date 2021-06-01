55acc468a419e0f271cb2e7eaab3723898ee3760
Pau Lòpez Pujals <iespaulopez@gmail.com>
2021-06-01 18:50:11 +0200

    Create DNA class

--- cellMitosis.js
+++ cellMitosis.js
@@ -1,36 +1,33 @@
+class DNA {
+  constructor(vel, size, color, mitChance, mutChance, mutIntensity, lifespan){
+    this.size = size;
+    this.vel = vel;
+    this.mutChance = mutChance;
+    this.mutIntensity = mutIntensity;
+    this.color = color;
+    this.mitChance = mitChance;
+    this.lifespan = lifespan;
+  }
+}
+
 class Cell {
-  constructor(id, x, y, vel, size, r, g, b, chance, mutChance, mutIntensity, deathtime) {
+  constructor(id, x, y, dna) {
     this.id = id;
     this.x = x;
     this.y = y;
-    this.size = size;
-    this.vel = vel;
-    this.chance = chance;
-    this.mutChance = mutChance;
-    this.mutIntensity = mutIntensity;
-    this.deathtime = deathtime;
-    this.r = r;
-    this.g = g;
-    this.b = b;
+    this.dna = dna;
   }
 
   mitosis() {
-    let newR = this.r;
-    let newG = this.g;
-    let newB = this.b;
+    let colorMut = this.dna.color;
     
-    let newSize = this.size;
-    let newChance = this.chance;
-    let newmutChance = this.mutChance;
+    let sizeMut = this.dna.size;
+    let mitChanceMut = this.dna.chance;
     
     if (random(1) < this.mutChance) {
-      newR = int(random(255));
-      newG = int(random(255));
-      newB = int(random(255));
-      newSize += random(-1, 1) * random(this.mutIntensity);
-      newChance += random(-1, 1) * random(this.mutIntensity)/100;
-      newmutChance += random(-1, 1) * random(this.mutIntensity)/100;
-      print(`R: ${this.r} G: ${this.g} B: ${this.b}`);
+      colorMut = [random(255), random(255), random(255)];
+      sizeMut += random(-1, 1) * random(this.dna.mutIntensity);
+      mitChanceMut += random(-1, 1) * random(this.dna.mutIntensity)/100;
       print("A Cell has mutated");
     }
 
@@ -38,13 +35,13 @@ class Cell {
     let xoffset = 0;
     let yoffset = 0;
     if (dir < 0.25) {
-      xoffset = this.size;
+      xoffset = this.dna.size;
     } else if (dir < 0.5) {
-      xoffset = -this.size;
+      xoffset = -this.dna.size;
     } else if (dir < 0.75) {
-      yoffset = this.size;
+      yoffset = this.dna.size;
     } else {
-      yoffset = -this.size;
+      yoffset = -this.dna.size;
     }
 
     let overlap = false;
@@ -55,21 +52,23 @@ class Cell {
       if (cells[i].x == x+xoffset && cells[i].y == y+yoffset) overlap = true;
     }*/
     if (!overlap) {
-      cells[cellnum++] = new Cell(this.id, this.x+xoffset, this.y+yoffset, this.vel, newSize, newR, newG, newB, newChance, newmutChance, this.mutIntensity, this.deathtime);
+      let dna = new DNA(this.dna.vel, sizeMut, colorMut, mitChanceMut, this.dna.mutChance, this.dna.mutIntensity, this.dna.lifespan);
+      cells[cellnum++] = new Cell(this.id, this.x+xoffset, this.y+yoffset, dna);
     }
   }
 
   tick() {
-    if(this.deathtime < this.lifetime){
+    if(this.dna.lifespan < this.lifetime){
       return;
     }
     
-    this.x += random(-this.vel, this.vel);
-    this.y += random(-this.vel, this.vel);
+    this.x += random(-this.dna.vel, this.dna.vel);
+    this.y += random(-this.dna.vel, this.dna.vel);
 
-    if (random(1) < this.chance) {
+    if (random(1) < this.dna.mitChance) {
       this.mitosis();
     }
+
     constrain(this.x, 0, width);
     constrain(this.y, 0, height);
     
@@ -77,15 +76,16 @@ class Cell {
   }
 
   draw() {
-    if(this.deathtime < this.lifetime){
+    if(this.dna.lifespan < this.lifetime){
+      print("dead");
       return;
     }
     noStroke();
-    fill(this.r, this.g, this.b, 150);
-    circle(this.x, this.y, this.size);
-    stroke(this.r, this.g, this.b, 200);
-    fill(this.r, this.g, this.b, 200);
-    circle(this.x, this.y, this.size/4);
+    fill(this.dna.color[0], this.dna.color[1], this.dna.color[2], 150);
+    circle(this.x, this.y, this.dna.size);
+    stroke(this.dna.color[0], this.dna.color[1], this.dna.color[2], 200);
+    fill(this.dna.color[0], this.dna.color[1], this.dna.color[2], 200);
+    circle(this.x, this.y, this.dna.size/4);
   }
 }
 
@@ -96,7 +96,7 @@ let cellnum = 0;
 function setup() {
   createCanvas(640, 480);
   
-  spawnCells(1, 30, 50, 255, 50, 5, 0.004, 0.03, 10, 60*10, 5000);
+  spawnCells(1, 30, [50, 255, 50], 5, 0.004, 0.03, 10, 60*10);
 }
 
 function draw() {
@@ -112,10 +112,11 @@ function draw() {
   text(int(cellnum), 0, 12*2);
 }
 
-function spawnCells(num, size, r, g, b, vel, chance, mutChance, mutIntensity, deathtime, offset) {
+function spawnCells(num, size, color, vel, chance, mutChance, mutIntensity, lifespan) {
   cellnum = num;
   for (let i = 0; i < num; i++) {
-    cells[i] = new Cell(i, width/2, height/2, vel, size, r, g, b, chance, mutChance, mutIntensity, deathtime);
+    let dna = new DNA(vel, size, color, chance, mutChance, mutIntensity, lifespan);
+    cells[i] = new Cell(i, width/2, height/2, dna);
   }
 }
 
