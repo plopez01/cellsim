b2ece770630f7247761c9faf7e26d536f586756d
Pau Lòpez Pujals <iespaulopez@gmail.com>
2021-06-01 19:50:16 +0200

    I have no idea why it's not working as I want

--- cellMitosis.js
+++ cellMitosis.js
@@ -8,6 +8,13 @@ class DNA {
     this.mitChance = mitChance;
     this.lifespan = lifespan;
   }
+
+  mutate(){
+    this.color = [random(255), random(255), random(255)];
+    this.size = abs(randomGaussian(this.size, this.mutIntensity));
+    this.mitChance = abs(randomGaussian(this.mitChance, this.mutIntensity/1000));
+    print(`Cell mutated with new DNA: Color=${this.color}, Size=${this.size}, MitChance=${this.mitChance}, MutChance=${this.mutChance}`);
+  }
 }
 
 class Cell {
@@ -16,19 +23,14 @@ class Cell {
     this.x = x;
     this.y = y;
     this.dna = dna;
+    this.lifetime = 0;
   }
 
   mitosis() {
-    let colorMut = this.dna.color;
-    
-    let sizeMut = this.dna.size;
-    let mitChanceMut = this.dna.chance;
-    
-    if (random(1) < this.mutChance) {
-      colorMut = [random(255), random(255), random(255)];
-      sizeMut += random(-1, 1) * random(this.dna.mutIntensity);
-      mitChanceMut += random(-1, 1) * random(this.dna.mutIntensity)/100;
-      print("A Cell has mutated");
+    let dnaCopy = this.dna;
+
+    if (random(1) < this.dna.mutChance) {
+      dnaCopy.mutate();
     }
 
     let dir = random(1);
@@ -48,12 +50,11 @@ class Cell {
 
     if ((this.x+xoffset < 0 || this.x+xoffset > width) || (this.y+yoffset < 0 || this.y+yoffset > height)) return;
 
-    /*for (int i = 0; i < cellnum; i++) {
-      if (cells[i].x == x+xoffset && cells[i].y == y+yoffset) overlap = true;
-    }*/
+    for (let i = 0; i < cellnum; i++) {
+      if (cells[i].x == this.x+xoffset && cells[i].y == this.y+yoffset) overlap = true;
+    }
     if (!overlap) {
-      let dna = new DNA(this.dna.vel, sizeMut, colorMut, mitChanceMut, this.dna.mutChance, this.dna.mutIntensity, this.dna.lifespan);
-      cells[cellnum++] = new Cell(this.id, this.x+xoffset, this.y+yoffset, dna);
+      cells[cellnum++] = new Cell(this.id, this.x+xoffset, this.y+yoffset, dnaCopy);
     }
   }
 
@@ -62,22 +63,21 @@ class Cell {
       return;
     }
     
-    this.x += random(-this.dna.vel, this.dna.vel);
-    this.y += random(-this.dna.vel, this.dna.vel);
+    //this.x += random(-this.dna.vel, this.dna.vel);
+    //this.y += random(-this.dna.vel, this.dna.vel);
 
     if (random(1) < this.dna.mitChance) {
       this.mitosis();
     }
 
-    constrain(this.x, 0, width);
-    constrain(this.y, 0, height);
+    constrain(this.x, this.dna.size*2, width-this.dna.size*2);
+    constrain(this.y, this.dna.size*2, height-this.dna.size*2);
     
     this.lifetime++;
   }
 
   draw() {
     if(this.dna.lifespan < this.lifetime){
-      print("dead");
       return;
     }
     noStroke();
@@ -96,12 +96,12 @@ let cellnum = 0;
 function setup() {
   createCanvas(640, 480);
   
-  spawnCells(1, 30, [50, 255, 50], 5, 0.004, 0.03, 10, 60*10);
+  spawnCells(1, 30, [50, 255, 50], 5, 0.004, 0.004, 1, 60*15);
 }
 
 function draw() {
   background(0);
-  if(cellnum >= 300) cellnum = 0;
+  if(cellnum >= 600) cellnum = 0;
   for (let i = 0; i < cells.length; i++) {
     if(cells[i] == null) continue;
     cells[i].tick();
@@ -116,7 +116,7 @@ function spawnCells(num, size, color, vel, chance, mutChance, mutIntensity, life
   cellnum = num;
   for (let i = 0; i < num; i++) {
     let dna = new DNA(vel, size, color, chance, mutChance, mutIntensity, lifespan);
-    cells[i] = new Cell(i, width/2, height/2, dna);
+    cells[i] = new Cell(i, random(width), random(height), dna);
   }
 }
 
